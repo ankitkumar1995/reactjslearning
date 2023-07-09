a81ef10a15c7665fe46441cb2a8856b61884fd4d
ankitkumar1995 <ankitknit15@gmail.com>
2023-07-09 16:41:12 +0530

    instalation

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -6,6 +6,7 @@ const AppRouter=()=>{
     return(
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/user" element={<User />}/>
         </Routes>
     )
 }
